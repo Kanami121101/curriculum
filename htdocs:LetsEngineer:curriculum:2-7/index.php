b8ae8afa8a7a2dabc8644501484b994830592cf3
Kanami121101 <wkmtsu.knami@icloud.com>
2022-06-15 15:25:50 +0900

    PHP課題配列、連想配列改訂版
    課題の評価お願いいたします。

--- htdocs:LetsEngineer:curriculum:2-7/index.php
+++ htdocs:LetsEngineer:curriculum:2-7/index.php
@@ -4,4 +4,15 @@ var_dump($colors);
 echo '<br>';
 $colors["yellow"]="黄色";
 var_dump($colors);
+echo '<br>'
+$list="プルリクエスト＝開発者のローカルリポジトリでの変更を他の開発者に通知する機能";
+echo $list;
+echo'<br>';
+
+$git_flow="git_flowとは、Gitにおけるリポジトリの分岐モデルであり、リールのことを指す";
+echo $git_flow;
+echo '<br>';
+
+$cron="cronとは、LInuxサーバー管理の一種で、大切な設定方法の一種";
+echo $cron;
 ?>
