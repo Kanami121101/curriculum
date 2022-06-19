1d3f3741feb11219f707057ebc7636e48b86c7c7
Kanami121101 <wkmtsu.knami@icloud.com>
2022-06-19 15:59:08 +0900

    2-11の課題提出
    課題が完了しましたので、提出します。

---
+++ htdocs:LetsEngineer:curriculum:2-11/index.php
@@ -0,0 +1,15 @@
+<form action="result.php" method="post">
+  名前：<input type="text" name="my_name" />
+  <br>
+  メールアドレス：<input type="text" name="email"/>
+  <br>
+  パスワード：<input type="password" name="password" />
+  <br>
+  <input type="submit" value="送信" />
+  <br>
+  「仕様書」は完成イメージを明確にした資料であるのに対し、「設計書」は完成するまでの制作工程を明確にした資料。
+  <br>
+  Gitとは、分散型バージョン管理システムで、ファイルのバージョン管理が簡単にできるツール
+  <br>
+  マージツールとは、複数のファイルやデータ、プログラムなどを、決められたルールに従って一つに統合すること。
+</form>
