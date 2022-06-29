f0f695661335733f6c8bf4e3d3bb1d7053114ffd
Kanami121101 <wkmtsu.knami@icloud.com>
2022-06-29 17:04:22 +0900

    第２章まとめテスト提出
    第２章まとめテストの提出をします。

--- 2-18/question.php
+++ 2-18/question.php
@@ -7,27 +7,27 @@ $name =$_POST['name'];
     <h2>①ネットワークのポート番号は何番？</h2>
 <?php 
     $number=["80","22","20","21"];
-    $answer="80";
+    $answer=$number[0];
 foreach($number as $value):?>
     <span><input type="radio" name="number" value="<?php echo $value?>"  /><?php echo $value?></span>
-   <?php endforeach?>
+    <?php endforeach?>
    <input type="hidden" name="answer" value="<?php echo $answer ?>">
    <h2>②Webページを作成するための言語は？</h2>
 <?php 
     $language=["PHP","Python","JAVA","HTML"];
-    $answer2="HTML";
+    $answer2=$language[3];
 foreach($language as $value):?>
     <span><input type="radio" name="language" value="<?php echo $value?>"  /><?php echo $value?></span>
     <?php endforeach?>
-    <input type="hidden" name="answer" value="<?php echo $answer2 ?>">
+    <input type="hidden" name="answer2" value="<?php echo $answer2 ?>">
     <h2>③MySQLで情報を取得するためのコマンドは？</h2>
 <?php 
      $my_sql=["join","select","insert","update"];
-     $answer3="select";
+     $answer3=$my_sql[1];
 foreach($my_sql as $value):?> 
     <span><input type="radio" name="my_sql" value="<?php echo $value?>"  /><?php echo $value?></span>
     <?php endforeach?>
-    <input type="hidden" name="answer2" value="<?php echo $answer3 ?>">
+    <input type="hidden" name="answer3" value="<?php echo $answer3 ?>">
     <br>
     <input type="submit" value="回答する">
 </form>
